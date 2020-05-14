a3aa451dcd978df06d27f0f1f8ce4ce90e192737
balzard <stephane.leblanc@student.unamur.be>
2020-05-14 17:58:19 +0200

    nice buttons + error handler

--- app/routes.py
+++ app/routes.py
@@ -87,6 +87,10 @@ createMatch()
 # Public section   #
 ####################
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
 #Home page
 @app.route("/")
 def home():
